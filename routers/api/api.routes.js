11063bcf05a5a7ea43b1ed089d29afe3053a07e7
Gabriel Surculento <gespejo121@gmail.com>
2022-03-25 13:35:18 -0300

    Object Process - Crando ruta 'api/randoms' 2

--- routers/api/api.routes.js
+++ routers/api/api.routes.js
@@ -8,14 +8,14 @@ router.use('/auth', authRoutes);
 
 router.get('/randoms', (req, res) => {
   const { cant } = req.query
-  console.log(cant)
+  console.log(cant,1)
   const random = fork(path.resolve(__dirname, './random/random.routes.js'))
 
-  random.send('start');
-  random.on('message', dato => {
-    res.send(`El resultado es => ${dato}`);
-  })
   random.send({cant: cant})
+  random.on('message', dato => {
+    // console.log(dato)
+    res.render('random',{numbers: dato});
+  })
 })
 
 module.exports = router;
--- routers/info/info.routes.js
+++ routers/info/info.routes.js
@@ -1,7 +1,16 @@
-
-
+const args = require('../../utils/args.utils')
 const info = (req,res) => {
-  req.params
+  const data = {
+    argumentos : args,
+    sistema : process.platform,
+    version : process.version,
+    memoria: process.memoryUsage().rss,
+    path : process.execPath,
+    processId : process.pid,
+    directorio : process.cwd(),
+  }
+  console.log(data)
+  res.render('info', {data:data})
 }
 
 module.exports = info
