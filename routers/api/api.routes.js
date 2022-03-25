f86ba0d6ce50042718aea108e5201cf734b38e19
Gabriel Surculento <gespejo121@gmail.com>
2022-03-25 14:11:32 -0300

    Object Process - Completo 1

--- routers/api/api.routes.js
+++ routers/api/api.routes.js
@@ -8,7 +8,7 @@ router.use('/auth', authRoutes);
 
 router.get('/randoms', (req, res) => {
   const { cant } = req.query
-  console.log(cant,1)
+  // console.log(cant,1)
   const random = fork(path.resolve(__dirname, './random/random.routes.js'))
 
   random.send({cant: cant})
--- routers/api/random/random.routes.js
+++ routers/api/random/random.routes.js
@@ -1,5 +1,5 @@
 
-const randomNumber = (cant = 10e7) => {
+const randomNumber = (cant = 1e8) => {
   const arr = {}
   for (let i = 0; i < cant; i++) {
     // 1 - 1000
@@ -10,11 +10,11 @@ const randomNumber = (cant = 10e7) => {
       arr[num] = 1
     }    
   }
-  console.log(arr)
+  // console.log(arr)
   return arr
 }
 process.on('message', (data) => {
-  console.log(data.cant,2)
+  // console.log(data.cant,2)
   const cant = isNaN(data.cant)? undefined : Number(data.cant)
   const arr = randomNumber(cant);
   process.send(arr);
